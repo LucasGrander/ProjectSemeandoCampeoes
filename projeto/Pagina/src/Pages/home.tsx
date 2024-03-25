c4c2c5496cbd39a6f9eb0c70763b36478cacbb9b
Lucas Grander <lucasgrander@gmail.com>
2024-03-25 19:06:17 -0300

    Commit 1.3 - Home Update
    Alterações na page>HOME, porém, sem o uso de componentes.

---
+++ projeto/Pagina/src/Pages/home.tsx
@@ -0,0 +1,51 @@
+import './home.css'
+import LogoSemeandoCampeoes from "../assets/LogoSemeandoCampeoes.png"
+import LogoInstagram from "../assets/facebook.svg"
+import LogoFacebook from "../assets/instagram.svg"
+import LogoYoutube from "../assets/youtube.svg"
+import UserLogin from "../assets/userICON.svg"
+import Camera from "../assets/camPHOTO.svg"
+import HandShake from "../assets/handShake.svg"
+
+const Home = () => {
+
+    return(
+        <>
+            <header>
+                <div className="logo">
+                    <a href='#'><img src={LogoSemeandoCampeoes} alt="Logo Semeando Campeões"></img></a>
+                </div>
+                <div className="title">
+                    <h1>Semeando Campeões</h1>
+                </div>
+                <div className="redes-sociais">
+                    <a href="#"><img src={LogoInstagram} alt="logo do instagram"></img></a>
+                    <a href="#"><img src={LogoFacebook} alt="logo do facebook"></img></a>
+                    <a href="#"><img className='youtube' src={LogoYoutube} alt="logo do youtube"></img></a>
+                </div>
+                <div className="login-acess">
+                    <a href="#"><img src={UserLogin} alt="user login"></img></a>
+                </div>
+            </header>
+            <nav>
+                <button>Galeria de Fotos<img src={Camera}></img></button>
+                <button className='btn1'>Participe!<img src={HandShake}></img></button>
+            </nav>
+            <div className="container">
+                <div className="content">
+                    <div className="title-container">
+                        <span>Sobre nós</span>
+                    </div>
+                    <div className="info-button">
+                        <button>Instrutores</button>
+                        <button>CT's</button>
+                        <button>Histórico</button>
+                    </div>
+                </div>
+            </div>
+        </>
+        
+    )
+}
+
+export default Home
