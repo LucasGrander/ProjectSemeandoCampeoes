e97c69629d376a0a95e37939470d82d27e19ffb4
LucasGrander <157768888+LucasGrander@users.noreply.github.com>
2024-04-08 11:30:25 -0300

    redes sociais dos instrutores com ICONES

--- ProjetoIntegrador/Pagina/src/components/Instrutores.tsx
+++ ProjetoIntegrador/Pagina/src/components/Instrutores.tsx
@@ -3,6 +3,10 @@ import DionatanIMG from '../assets/FotoDionatan.png'
 import RicardoIMG from '../assets/FotoRicardo.png'
 import DanielIMG from '../assets/Daniel.jpg'
 
+import instaLOGO from '../assets/instagram.svg'
+import faceLOGO from '../assets/facebook.svg'
+import youtubeLOGO from '../assets/youtube.svg'
+
 interface Props{
     id?: string;
 }
@@ -21,7 +25,13 @@ const Instrutores = ({id}: Props) => {
                 <span>Daniel Bruno da Silva Ramos</span>
                 <p className='desc-breve'>Faixa Preta de Jiu Jitsu, instrutor certificado pela Gracie Barra BJJ.</p>
                 <p className='desc-instrutor'>Professor responsável pela Gracie Barra Campo Mourão, Presidente da Associação Mourãoense de Jiu Jitsu, Professor responsável pelo PSC, com vários títulos em Campeonatos de Jiu jitsu do Rio de Janeiro, Paraná e Brasileiros com e sem quimono.</p>
+                <div className="redes-sociais-instrutor">
+                    <a href='/'><img src={instaLOGO}></img></a> 
+                    <a href='/'><img src={youtubeLOGO}></img></a> 
+                    <a href='/'><img src={faceLOGO}></img></a> 
+                </div>
             </div>
+            
         </div>
 
         <div className="box">
@@ -32,6 +42,11 @@ const Instrutores = ({id}: Props) => {
                 <span>Ricardo Maciel Batista</span>
                 <p className='desc-breve' >Faixa Preta de Jiu Jitsu, Faixa azul de Judô, com Bacharelado e Licenciatura em Educação Física</p>
                 <p className='desc-instrutor'>Instrutor certificado pela Gracie Barra BJJ, Multiplicador institucional de Atendimento Pré-Hospitalar (APH) Tático pela Polícia Militar do Parana, APH/MARC 1 pela Polícia Civil do Paraná e Polícia Rodoviária Federal.</p>
+                <div className="redes-sociais-instrutor">
+                    <a href='/'><img src={instaLOGO}></img></a> 
+                    <a href='/'><img src={youtubeLOGO}></img></a> 
+                    <a href='/'><img src={faceLOGO}></img></a> 
+                </div>
             </div>
         </div>
 
@@ -43,6 +58,11 @@ const Instrutores = ({id}: Props) => {
                 <span>Dionata da Silva Benedito</span>
                 <p className='desc-breve' > Faixa Marrom de Jiu Jitsu, Faixa laranja de Judô, instrutor certificado pela Gracie Barra BJJ e curso de primeiros socorros básico.</p>
                 <p className="desc-instrutor">Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda quasi libero laboriosam ipsum neque at, reprehenderit facere cum, harum, consequuntur iusto placeat omnis quis fugiat debitis. Beatae veniam repellendus architecto.</p>
+                <div className="redes-sociais-instrutor">
+                    <a href='/'><img src={instaLOGO}></img></a> 
+                    <a href='/'><img src={youtubeLOGO}></img></a> 
+                    <a href='/'><img src={faceLOGO}></img></a> 
+                </div>
             </div>
         </div>
     </div>
