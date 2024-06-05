03e4ec5e33a97c3a02f42d8312e2d14b3c7a2f2e
LucasGrander <157768888+LucasGrander@users.noreply.github.com>
2024-06-05 11:24:59 -0300

    layout teste (newsPage) --> ajustes responsividade

--- ProjetoIntegrador/Pagina/src/Pages/NewsLatter.tsx
+++ ProjetoIntegrador/Pagina/src/Pages/NewsLatter.tsx
@@ -25,51 +25,64 @@ const NewsLatter = ({id}: Props) => {
   }
 
   return (
-    <div id={id} className="content-history">
+    <div id={id} className="page-news">
       <div style={{display: overlay? 'flex' : 'none' }} className="overlay">
-            <img className='loading-top' src={overlayLoadingCircle}></img>
-            <img className='loading-logo' src={LogoSemeandoCampeoes}></img>
+        <img className='loading-top' src={overlayLoadingCircle}></img>
+        <img className='loading-logo' src={LogoSemeandoCampeoes}></img>
       </div>
-        <header>
-            <div className="logo">
-                <img onClick={handleOnClickHome} src={LogoSemeandoCampeoes} alt="Logo Semeando Campeões"></img>
-            </div>
-            <div className="title">
-                <h1>Boletim Informativo BJJ CM</h1>
+      <header>
+          <div className="logo">
+              <img onClick={handleOnClickHome} src={LogoSemeandoCampeoes} alt="Logo Semeando Campeões"></img>
+          </div>
+          <div className="title">
+                <h2>Informativo</h2>
+                <span className="subtitle">Brazillian Jiu-Jitsu</span>
             </div>
         </header>
       
-        <div className="box-history">
-            <div className="text-history">
-                <h2>Novo CT do PSC "Asa Leste"</h2>
-                Em 28 de maio a instrutora faixa marrom Néia iniciou as aulas nas dependências da Escola Estadual Ivone Castanharo das 19h as 20h. Os treinos serão ministrados todas as 
-                terças e quintas das 19h as 20h para 20 adolescentes selecionados entre os alunos da escola.
+        <div className="content-news">
+            <div className="container-news">
+                <div className="container-title-news">
+                    <span >Últimas Notícas</span>
+                </div>
 
-              <h5>Imagens: <a target='_blank'  href="https://youtu.be/ydjqrB2vcwg?si=upTl7bcXO9DrBS6D">1º Treino CT Asa Leste</a></h5>
-              <h5>Postagem 03/06/2024</h5>
-              <br></br>
 
-              
-              <h2>Copa Noroeste de Judô</h2>
-              Campo Mourão recebeu no fim de semana dos dias 25 e 26 de maio, a maior competição de judô da história do Paraná, o Campeonato Paranaense de Judô. O evento foi realizado pela 
-              Associação Mourãoense de Judô (AMJ), em parceria com a Prefeitura de Campo Mourão, por meio da Fundação de Esportes (FECAM) e a Federação Paranaense de Judô (FPRJ) e os 
-              apoiadores do evento e do judô mourãoense de 2024.<br></br>
-              As categorias sub-13, sub-15, sub-21 e veterano competiram no sábado (25), das 8h às 18h, enquanto o Torneio Regional de Judô – 04 anos acima, foi realizado no domingo (26), das 8h às 14h, 
-              ambos no Ginásio de Esportes Belin Carolo, em anexo a Universidade Tecnológica Federal do Paraná (UFPR) – Campus de Campo Mourão.
-              <h5>Mais informações: <a target='_blank'  href="https://resenhacm.com.br/campo-mourao-recebe-campeonato-paranaense-de-judo-neste-final-de-semana-no-ginasio-da-utfpr/">site resenha CM</a></h5> 
-              
-              <h5>Álbum de Fotografias - GOOGLE FOTOS <a target='_blank'  href="https://photos.app.goo.gl/GqvAzULfFH5pLWtH7">*Torneio Regional Noroeste*</a> - 26/05/2024 - Campo Mourão - Andrré Fedalto - Vitor Albres.</h5>
-              <h5>Postagem 27/05/2024</h5>
-                          
-              <h2>Treinão solidário 2024</h2>
-              Em 24/05/2024 as equipes PSC, Gracie Barra, Moraes BJJ (Norte) e Guedes BJJ (Clube feijão) se reuniram no Uchoa Dojô para realizar um treino solidário para arrecadação de ítens de higiene 
-              pessoal para as vítimas da enxente no Rio Grande do Sul. Neste dia foram ministradas técnicas diversas pelos professores Daniel Ramos e João Guedes.
+                <div className="box-news">
+                    <span className='title-news' >Novo CT do PSC "Asa Leste"</span>
 
-              <h5>Vídeo: <a target='_blank'  href="https://youtu.be/ydjqrB2vcwg?si=upTl7bcXO9DrBS6D">Treinão Solidário 2024</a></h5>
-              <h5>Postagem 27/05/2024</h5>
-              <br></br>
-              
+                    <span className='text-news' ><span className='parag-visible0' >..........</span>No dia 28 de maio, a instrutora faixa marrom, Ednéia de Souza, iniciou as aulas nas dependências da Escola Estadual Ivone Castanharo, das 19h às 20h. Os treinos serão ministrados todas as terças e quintas, das 19h às 20h, para 20 adolescentes selecionados entre os alunos da escola.</span>
+
+                    <span className='midia-news' >Imagens: <a target='_blank'  href="https://youtu.be/ydjqrB2vcwg?si=upTl7bcXO9DrBS6D">1º Treino CT Asa Leste</a></span>
+
+                    <span className='data-news' >03/06/2024</span>
+                </div>
+
+
+                <div className="box-news">
+                    <span className='title-news' >Copa Noroeste de Judô</span>
+
+                    <span className='text-news' ><span className='parag-visible0' >..........</span>Campo Mourão recebeu, no fim de semana dos dias 25 e 26 de maio, a maior competição de judô da história do Paraná, o Campeonato Paranaense de Judô. O evento foi realizado pela Associação Mourãoense de Judô (AMJ), em parceria com a Prefeitura de Campo Mourão, por meio da Fundação de Esportes (FECAM) e da Federação Paranaense de Judô (FPRJ), além dos apoiadores do evento e do judô mourãoense de 2024. As categorias sub-13, sub-15, sub-21 e veterano competiram no sábado (25), das 8h às 18h, enquanto o Torneio Regional de Judô – 04 anos acima foi realizado no domingo (26), das 8h às 14h, ambos no Ginásio de Esportes Belin Carolo, anexo à Universidade Tecnológica Federal do Paraná (UTFPR) – Campus de Campo Mourão.</span>
+
+                    <span className='midia-news'>Álbum de Fotografias(Google Fotos): <a target='_blank'  href="https://photos.app.goo.gl/GqvAzULfFH5pLWtH7">*Torneio Regional Noroeste*</a> - Campo Mourão - André Fedalto & Vitor Albres.</span>
+
+                    <span>Mais informações: <a target='_blank'  href="https://resenhacm.com.br/campo-mourao-recebe-campeonato-paranaense-de-judo-neste-final-de-semana-no-ginasio-da-utfpr/"> Resenha CM</a></span>
+
+                    <span className='data-news' >27/05/2024</span>
+                </div>
+
+
+                <div className="box-news">
+                    <span className='title-news' >Treinão solidário 2024"</span>
+
+                    <span className='text-news' ><span className='parag-visible0' >..........</span>Em 24/05/2024 as equipes PSC, Gracie Barra, Moraes BJJ (Norte) e Guedes BJJ (Clube feijão) se reuniram no Uchoa Dojô para realizar um treino solidário para arrecadação de ítens de higiene 
+                pessoal para as vítimas da enxente no Rio Grande do Sul. Neste dia foram ministradas técnicas diversas pelos professores Daniel Ramos e João Guedes.</span>
+
+                    <span className='midia-news'>Vídeo: <a target='_blank'  href="https://youtu.be/ydjqrB2vcwg?si=upTl7bcXO9DrBS6D">Treinão Solidário 2024</a></span>
+
+                    <span className='data-news' >27/05/2024</span>
+                </div>
             </div>
+
         </div>
         <Footer />
           
